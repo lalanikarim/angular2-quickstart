2660cf5a5effdd16764f30de3069160ff6129f7f
Karim Lalani <karim@karim-lalani.com>
2016-09-21 16:17:44 -0500

    Listing heroes with ngFor

--- app/app.component.ts
+++ app/app.component.ts
@@ -24,8 +24,8 @@ const HEROES: Hero[] = [
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li>
-      <!-- each hero goes here -->
+      <li *ngFor="let hero of heroes">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
     <h2>{{hero.name}} details!</h2>
