29f26dbc63471a45383e6ab1120d2f0b82b1cf1e
Karim Lalani <karim@karim-lalani.com>
2016-09-21 15:41:20 -0500

    Show our Hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
 })
-export class AppComponent { }
+
+export class AppComponent { 
+  title= 'Tour of Heroes';
+  hero= 'Windstorm';
+}
