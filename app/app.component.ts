a05a260e1a345c5e7c3a3cf019c6b5ab7e65586f
Karim Lalani <jimmy00784@gmail.com>
2016-09-22 01:27:13 -0500

    The ngOnInit Lifecycle Hook. (and mock-heroes.ts export fix)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
@@ -70,8 +71,12 @@ import { HeroService } from './hero.service';
   providers: [ HeroService ]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(private heroService: HeroService){}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   title= 'Tour of Heroes';
   heroes: Hero[];
--- app/mock-heroes.ts
+++ app/mock-heroes.ts
@@ -1,7 +1,7 @@
 import { Hero } from './hero';
 
 
-const HEROES: Hero[] = [
+export const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
