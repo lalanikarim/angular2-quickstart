90774dd132cb6788c305c050803f810fd26d3174
Karim Lalani <jimmy00784@gmail.com>
2016-09-22 01:19:40 -0500

    getHeroes in AppComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -76,6 +76,10 @@ export class AppComponent {
   title= 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
