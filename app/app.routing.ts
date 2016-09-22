33472da34e946511439a57a7ee55fa859a54b4b1
Karim Lalani <jimmy00784@gmail.com>
2016-09-22 16:29:18 -0500

    Configure a Route with a Parameter

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component';
 
 const appRoutes: Routes = [
@@ -12,6 +13,10 @@ const appRoutes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent
+  },
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
   },
   {
     path: '',
