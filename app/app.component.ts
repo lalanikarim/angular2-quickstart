495491ba0bb98acaeb7134c008decfd15daff2f8
Karim Lalani <jimmy00784@gmail.com>
2016-09-22 01:09:43 -0500

    Mock Heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,19 +2,6 @@ import { Component } from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
 @Component({
   selector: 'my-app',
   styles: [`
@@ -83,7 +70,7 @@ const HEROES: Hero[] = [
 
 export class AppComponent { 
   title= 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
   
   onSelect(hero: Hero): void {
