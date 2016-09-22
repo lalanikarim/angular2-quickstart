ce89e9bd35544af3c313e8d11871dfd0ccb43e52
Karim Lalani <jimmy00784@gmail.com>
2016-09-22 17:09:10 -0500

    Select a Dashboard Hero

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
@@ -10,11 +11,14 @@ import { HeroService } from './hero.service';
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService:HeroService){ }
+  constructor(private router:Router, private heroService:HeroService){ }
 
   ngOnInit():void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1,5));
   }
 
-  getDetail(hero: Hero): void { /* not implemented yet */ }
+  getDetail(hero: Hero): void {
+    let link = ['/detail',hero.id];
+    this.router.navigate(link);
+  }
 }
