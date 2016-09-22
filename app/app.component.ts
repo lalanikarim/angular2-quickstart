ddf2f7570e1932c225e2c6cc6cb810580f009557
Karim Lalani <jimmy00784@gmail.com>
2016-09-22 14:38:15 -0500

    Router Links

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
+    <a routerLink="/heroes">Heroes</a>
     <router-outlet></router-outlet>
   `
 })
