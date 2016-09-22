9944a5d673069b82493461cbd1280eae29e48f5b
Karim Lalani <jimmy00784@gmail.com>
2016-09-22 17:13:23 -0500

    Add the mini-detail

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -65,7 +65,12 @@ import { HeroService } from './hero.service';
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+    <div *ngIf="selectedHero">
+      <h2>
+	{{selectedHero.name | uppercase}} is my hero
+      </h2>
+      <button (click)="gotoDetail()">View Details</button>
+    </div>
     `
 })
 
