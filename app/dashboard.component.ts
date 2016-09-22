667b4bd58683903bf92c74bb0e1890e0d51af2eb
Karim Lalani <jimmy00784@gmail.com>
2016-09-22 14:51:23 -0500

    Dashboard Top Heroes

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'my-dashboard',
-  template: '<h3>My Dashboard</h3>'
+  templateUrl: 'app/dashboard.component.html'
 })
 
 export class DashboardComponent {}
